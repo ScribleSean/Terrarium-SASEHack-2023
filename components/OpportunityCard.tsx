93a416eba749b005e635b2ab8e435ec7bdd4a757
Connor Tam <connor0530@gmail.com>
2023-10-07 18:41:00 -0400

    fix: hydration error

--- components/OpportunityCard.tsx
+++ components/OpportunityCard.tsx
@@ -1,25 +1,16 @@
 import Link from "next/link";
 
-export default function OpportunityCard(props: {
-  opportunity: Opportunity;
-  onClick?: (id: string) => void;
-}) {
+export default function OpportunityCard(props: { opportunity: Opportunity }) {
   const { opportunity } = props;
 
   return (
-    <div
-      className="card m-1"
-      style={{ width: "20rem", cursor: "pointer" }}
-      onClick={() => props.onClick?.(opportunity.id)}
-    >
-      <img src={opportunity.imageUrl} className="card-img-top" alt="..." />
+    <div className="card m-1" style={{ width: "20rem", cursor: "pointer" }}>
+      <img src={opportunity.imageUrl} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title">{opportunity.name}</h5>
         <p className="card-text m-0">📅 {opportunity.date}</p>
         <p className="card-text m-0">📍 {opportunity.location}</p>
-        <Link href={`/organizations/${opportunity.organizationId}`}>
-          🫂 {opportunity.organizationName}
-        </Link>
+        <p className="card-text m-0">🫂 {opportunity.organizationName}</p>
       </div>
     </div>
   );
