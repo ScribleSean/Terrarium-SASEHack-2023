45ad9dc5394bd00f9bc4d31cca47ead936a59e9e
scriblesean <sean.arackal@gmail.com>
2023-10-07 21:04:29 -0400

    server update
    login auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,45 +1,53 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
-import client from "../../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
+
+
 
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: { label: "Email", type: "text", placeholder: "email@company" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        (await client).db().collection("users").insertOne({
-          id: "id",
-          name: "test",
-          email: "test@test",
-        });
-        // // You need to provide your own logic here that takes the credentials
-        // // submitted and returns either a object representing a user or value
-        // // that is false/null if the credentials are invalid.
-        // // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // // You can also use the `req` object to obtain additional parameters
-        // // (i.e., the request IP address)
-        // const res = await fetch("/your/endpoint", {
-        //   method: "POST",
-        //   body: JSON.stringify(credentials),
-        //   headers: { "Content-Type": "application/json" },
-        // });
-        // const user = await res.json();
+      async authorize(credentials) {
+        if (!credentials) {
+          return Promise.resolve(null); // Return null if credentials are missing
+        }
+        try {
+          const client = await clientPromise;
+          const usersCollection = client.db().collection("users");
 
-        // // If no error and we have user data, return it
-        // if (res.ok && user) {
-        //   return user;
-        // }
-        // // Return null if user data could not be retrieved
+          // Check if a user with the provided username already exists
+          const existingUser = await usersCollection.findOne({
+            email: credentials.email,
+          });
 
-        return { id: "id", name: "test", email: "test@test" };
+          if (!existingUser || existingUser.password !== credentials.password) {
+            return null;
+          }
+
+          // valid credentials insert the new user
+          const result = await usersCollection.insertOne({
+            email: credentials.email,
+            password:credentials.password
+          });
+
+          // Return the newly created user
+          return{
+            id: result.insertedId.toString(),
+            email: credentials.email,
+            password: credentials.password,
+          };
+        } catch (error) {
+          return Promise.resolve(null); // Something went wrong, return null
+        }
       },
     }),
   ],
 };
-
 export default NextAuth(authOptions);
+
